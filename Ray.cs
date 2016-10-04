9b18007446150dfadc4da8cda233c14b2caa75bf
Lagzder <Lvalek4@gmail.com>
2016-10-04 13:45:03 +0200

    Update Ray.cs

--- Ray.cs
+++ Ray.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +8,13 @@ namespace Cross
 {
     class Ray
     {
+        public double a { get; set; }
+        public double b { get; set; }
+
+        public Ray(double a, double b)
+        {
+            this.a = a;
+            this.b = b;
+        }
     }
 }
