e7de966e2d916ec0ed401cb669130692d96eb782
Lagzder <Lvalek4@gmail.com>
2016-10-19 15:48:45 +0200

    Update RayTracer.cs

--- RayTracer.cs
+++ RayTracer.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,18 +8,34 @@ namespace Cross
 {
     class RayTracer
     {
-        public Vector canvasPoint = new Vector();
         public RayTracer() { }
 
-        public void CreateRay(double x1, double y1)
+        public Ray CreateRay(double x1, double y1)
         {
             x1 = x1 / 640;
             y1 = y1 / 480;
 
-            canvasPoint = new Vector (x1, y1, 0);
-            Vector dirVector = new Vector(x1, y1, 1);
+            Ray ray = new Ray();
+            return ray = new Ray(0, 0,-1 ,x1 ,y1 , 1);
+        }
 
-            Console.WriteLine("r(t) = <0, 0, -1> + t * <{0}, {1}, {2}>", dirVector.x, dirVector.y, dirVector.z);
+        public void Render(Sphere sphere)
+        {
+            for(int y = 0; y < 50; y++)
+            {
+                for(int x = 0; x <50; x++)
+                {
+                    if (sphere.GetIntersection(CreateRay(x * 12.8, y * 9.6)) == null) // pre zobrazenie 50 x 50 v konzole
+                    {
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.Write("*");
+                    }                      
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
