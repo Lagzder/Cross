e0e4dbdb119cb2611bae94919b4ab1a962175eaa
Lagzder <Lvalek4@gmail.com>
2016-11-03 11:00:36 +0100

    Add files via upload

---
+++ Pixel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cross
+{
+    class Pixel
+    {
+        public double x { get; set; }
+        public double y { get; set; }
+        public double intersectSize { get; set; }
+
+        public Pixel(double x, double y, double intersectSize)
+        {
+            this.x = x;
+            this.y = y;
+            this.intersectSize = intersectSize;
+        }
+    }
+}
