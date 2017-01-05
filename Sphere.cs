a56a21540d690d046f2ad84249ad94d64ea0e8fa
Lagzder <Lvalek4@gmail.com>
2017-01-05 11:43:18 +0100

    Add files via upload

--- Sphere.cs
+++ Sphere.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,27 +8,23 @@ namespace Cross
 {
     class Sphere : IObject
     {
-        public double xc { get; set; }
-        public double yc { get; set; }
-        public double zc { get; set; }
-        public double r  { get; set; }
+        Vector Centre = new Vector();
+        public double R  { get; set; }
 
-        public Sphere(double xc, double yc, double zc, double r) // sphere
+        public Sphere(Vector centre, double r) // sphere
         {
-            this.xc = xc;
-            this.yc = yc;
-            this.zc = zc;
-            this.r = r;
+            this.Centre = centre;
+            this.R = r;
         }
 
         public Sphere() { }
 
         public Vector GetIntersection(Ray ray)
         {
-            Vector intPoint = new Vector();
-            double a = Math.Pow(ray.vector.x, 2) + Math.Pow(ray.vector.y, 2) + Math.Pow(ray.vector.z, 2); //xd, yd, zd
-            double b = 2 * (ray.vector.x * (ray.point.x - xc) + ray.vector.y * (ray.point.y - yc) + ray.vector.z * (ray.point.z - zc));
-            double c = Math.Pow((ray.point.x - xc), 2) + Math.Pow((ray.point.y - yc), 2) + Math.Pow((ray.point.z - zc), 2) -Math.Pow(r, 2);
+            Vector intPixel = new Vector();
+            double a = Math.Pow(ray.vector.X, 2) + Math.Pow(ray.vector.Y, 2) + Math.Pow(ray.vector.Z, 2); //xd, yd, zd
+            double b = 2 * (ray.vector.X * (ray.point.X - Centre.X) + ray.vector.Y * (ray.point.Y - Centre.Y) + ray.vector.Z * (ray.point.Z - Centre.Z));
+            double c = Math.Pow((ray.point.X - Centre.X), 2) + Math.Pow((ray.point.Y - Centre.Y), 2) + Math.Pow((ray.point.Z - Centre.Z), 2) -Math.Pow(R, 2);
 
             double D = (b * b) - 4 * a * c;
            
@@ -42,11 +38,11 @@ namespace Cross
             {
                 double t = -b / 2 * a;
 
-                double x0 = ray.point.x + (t * ray.vector.x);
-                double y0 = ray.point.y + (t * ray.vector.y);
-                double z0 = ray.point.z + (t * ray.vector.z);
+                double x0 = ray.point.X + (t * ray.vector.X);
+                double y0 = ray.point.Y + (t * ray.vector.Y);
+                double z0 = ray.point.Z + (t * ray.vector.Z);
                 //Console.WriteLine("Intersection in 1 point x: {0}, y: {1}, z: {2}", x0, y0, z0);
-                return intPoint = new Vector(x0, y0, z0);
+                return intPixel = new Vector(x0, y0, z0);
 
 
             }
@@ -60,37 +56,37 @@ namespace Cross
                 {
                     if (t0 < t1)
                     {
-                        double x1 = ray.point.x + (t0 * ray.vector.x);
-                        double y1 = ray.point.y + (t0 * ray.vector.y);
-                        double z1 = ray.point.z + (t0 * ray.vector.z);
+                        double x1 = ray.point.X + (t0 * ray.vector.X);
+                        double y1 = ray.point.Y + (t0 * ray.vector.Y);
+                        double z1 = ray.point.Z + (t0 * ray.vector.Z);
                         //Console.WriteLine("Intersection\nx: {0}, y: {1}, z: {2}", x1, y1, z1);
-                        return intPoint = new Vector(x1, y1, z1);
+                        return intPixel = new Vector(x1, y1, z1);
                     }
                     else
                     {
-                        double x1 = ray.point.x + (t1 * ray.vector.x);
-                        double y1 = ray.point.x + (t1 * ray.vector.y);
-                        double z1 = ray.point.x + (t1 * ray.vector.z);
+                        double x1 = ray.point.X + (t1 * ray.vector.X);
+                        double y1 = ray.point.X + (t1 * ray.vector.Y);
+                        double z1 = ray.point.X + (t1 * ray.vector.Z);
                         //Console.WriteLine("Intersection\nx: {0}, y: {1}, z: {2}", x1, y1, z1);
-                        return intPoint = new Vector(x1, y1, z1);
+                        return intPixel = new Vector(x1, y1, z1);
                     }
                 }
 
                 if(t0 < 0 && t1 > 0)
                 {
-                    double x1 = ray.point.x + (t1 * ray.vector.x);
-                    double y1 = ray.point.x + (t1 * ray.vector.y);
-                    double z1 = ray.point.x + (t1 * ray.vector.z);
+                    double x1 = ray.point.X + (t1 * ray.vector.X);
+                    double y1 = ray.point.X + (t1 * ray.vector.Y);
+                    double z1 = ray.point.X + (t1 * ray.vector.Z);
                     //Console.WriteLine("Intersection\nx: {0}, y: {1}, z: {2}", x1, y1, z1);
-                    return intPoint = new Vector(x1, y1, z1);
+                    return intPixel = new Vector(x1, y1, z1);
                 }
                 if(t1 < 0 && t0 > 0)
                 {
-                    double x1 = ray.point.x + (t0 * ray.vector.x);
-                    double y1 = ray.point.y + (t0 * ray.vector.y);
-                    double z1 = ray.point.z + (t0 * ray.vector.z);
+                    double x1 = ray.point.X + (t0 * ray.vector.X);
+                    double y1 = ray.point.Y + (t0 * ray.vector.Y);
+                    double z1 = ray.point.Z + (t0 * ray.vector.Z);
                     //Console.WriteLine("Intersection\nx: {0}, y: {1}, z: {2}", x1, y1, z1);
-                    return intPoint = new Vector(x1, y1, z1);
+                    return intPixel = new Vector(x1, y1, z1);
                 }
 
                 if(t1 < 0 && t0 < 0)
--- Vector.cs
+++ Vector.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,58 +11,39 @@ namespace Cross
 {
     class Vector
     {
-        public double x { get; set; }
-        public double y { get; set; }
-        public double z { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
 
-        public double size => Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2) + Math.Pow(z, 2));
+        public double size => Math.Sqrt(Math.Pow(X, 2) + Math.Pow(Y, 2) + Math.Pow(Z, 2));
 
         public Vector(double x, double y, double z) // konstruktor
         {
-            this.x = x;
-            this.y = y;
-            this.z = z;
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
         }
 
         public Vector() { }
 
         public Vector u(Vector v) //normalizovany
         {
-            Vector u = new Vector();
-
-            u.x = (1 / v.size * v.x);
-            u.y = (1 / v.size * v.y);
-            u.z = (1 / v.size * v.z);
-            
-
-            return u;
+            return new Vector((1 / v.size * v.X), (1 / v.size * v.Y), (1 / v.size * v.Z));
         }
 
         public static Vector operator +(Vector a, Vector b) //scitanie
         {
-            Vector vector = new Vector();
-
-            vector.x = a.x + b.x;
-            vector.y = a.y + b.y;
-            vector.z = a.z + b.z;
-
-            return vector;
+            return new Vector((a.X + b.X), (a.Y + b.Y), (a.Z + b.Z));
         }
 
         public static Vector operator -(Vector a, Vector b) //odcitanie
         {
-            Vector vector = new Vector();
-
-            vector.x = a.x - b.x;
-            vector.y = a.y - b.y;
-            vector.z = a.z - b.z;
-
-            return vector;
+            return new Vector((a.X - b.X), (a.Y - b.Y), (a.Z - b.Z));
         }
 
         public static double operator *(Vector a, Vector b) //skalarny sucin
         {
-            return a.x * b.x + a.y * b.y + a.z * b.z;
+            return (a.X * b.X + a.Y * b.Y + a.Z * b.Z);
         }
     }
 }
